786edb21f85b655a82aaf030e38b0f55a7e26e2c
aomiki <aomiki.milky@icloud.com>
2025-07-07 02:59:31 +0400

    rework for checking each message id

--- Program.c
+++ Program.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include "system_types.h"
 #include "communication_types.h"
+#include "errno.h"
 
 void system_state_to_str(char* msg, SystemState state)
 {
@@ -78,55 +79,109 @@ void system_area_to_str(char* msg, SystemArea area)
 
 #define PERIPH_BIT_TO_STR(BIT_SEQUENCE, BIT_MASK) ( ((BIT_SEQUENCE) & (BIT_MASK)) != 0? PERIPH_STR_ON : PERIPH_STR_OFF )
 
+typedef enum
+{
+	STATE_UKNOWN,
+	STATE_ID_FIRST,
+	STATE_ID_SECOND
+} State;
+
+bool is_prev_skip = false;
+
 int main()
 {
 	uint8_t payload[TELEMETRY_BYTES_SIZE];
 	Telemetry decoded;
+	State curr_state = STATE_UKNOWN;
 
 	while (true)
 	{
-		memset(payload, 0, TELEMETRY_BYTES_SIZE);
-
-		if(fread(payload, 1, TELEMETRY_BYTES_SIZE, stdin) == TELEMETRY_BYTES_SIZE)
+		uint8_t curr_char = 0;
+		if (fread(&curr_char, 1, 1, stdin) != 1)
 		{
-			uint32_t time_ms;
-			uint16_t received_id = 0;
-			set_default_telemetry(&decoded);
-			bytes_to_telemetry(&decoded, &time_ms, &received_id, payload);
-
-			if (received_id != TELEMETRY_ID)
+			if (!is_prev_skip)
 			{
-				//Return back and skip 2 first bytes
-				fseek(stdin, -TELEMETRY_BYTES_SIZE + 1, SEEK_CUR);
-				printf("_skip_\n");
-				continue;
+				printf("_skip_\n\n");
+				is_prev_skip = true;
+			}
+		}
+
+		if (curr_state == STATE_UKNOWN)
+		{
+			if(curr_char == TELEMETRY_ID_UPPER)
+			{
+				curr_state = STATE_ID_FIRST;
+			}
+			else
+			{
+				curr_state = STATE_UKNOWN;
+				if (!is_prev_skip)
+				{
+					printf("_skip_\n\n");
+					is_prev_skip = true;
+				}
+			}
+		}
+		else if(curr_state == STATE_ID_FIRST)
+		{
+			if(curr_char == TELEMETRY_ID_LOWER)
+			{
+				curr_state = STATE_ID_SECOND;
+			}
+			else
+			{
+				curr_state = STATE_UKNOWN;
+				if (!is_prev_skip)
+				{
+					printf("_skip_\n\n");
+					is_prev_skip = true;
+				}
+			}
+		}
+		else if(curr_state == STATE_ID_SECOND)
+		{
+			memset(payload, 0, TELEMETRY_BYTES_SIZE);
+			//from previous iterations
+			payload[0] = TELEMETRY_ID_UPPER;
+			payload[1] = TELEMETRY_ID_LOWER;
+			payload[2] = curr_char;
+		
+			if(fread(payload+3, 1, TELEMETRY_BYTES_SIZE-3, stdin) == TELEMETRY_BYTES_SIZE-3)
+			{
+				uint32_t time_ms;
+				uint16_t received_id = 0;
+				set_default_telemetry(&decoded);
+
+				bytes_to_telemetry(&decoded, &time_ms, &received_id, payload);
+
+				char str_state[32];
+				system_state_to_str(str_state, decoded.sys_state);
+
+				char str_area[32];
+				system_area_to_str(str_area, decoded.sys_area);
+
+				char str_id[3];
+				memcpy(str_id, &received_id, 2);
+				str_id[2] = '\0';
+
+				is_prev_skip = false;
+				printf("\x1b[48;2;89;123;202m / id: %s / ms: %lu \x1b[48;2;184;55;177m/ state: %s /\x1b[0m\x1b[48;2;89;123;202m area: %s / temp: %f / pressure: %f / altitude: %f / acc: (%.4f, %.4f, %.4f) / gps: (alt %.4f, lat %.4f, lon %.4f)\x1b[0m\n", str_id, time_ms, str_state, str_area, decoded.temp, decoded.pressure, decoded.altitude, decoded.acc_x, decoded.acc_y, decoded.acc_z, decoded.gps.altitude, decoded.gps.latitude, decoded.gps.longitude);
+				printf("peripherals | radio: %s / sd: %s / barom: %s / acc: %s / gps: %s / jumper: %s / servo: %s\n\n",
+					PERIPH_BIT_TO_STR(decoded.sys_status, PERIPH_RADIO),
+					PERIPH_BIT_TO_STR(decoded.sys_status, PERIPH_SD),
+					PERIPH_BIT_TO_STR(decoded.sys_status, PERIPH_BAROM),
+					PERIPH_BIT_TO_STR(decoded.sys_status, PERIPH_ACC),
+					PERIPH_BIT_TO_STR(decoded.sys_status, PERIPH_GPS),
+					PERIPH_BIT_TO_STR(decoded.sys_status, PERIPH_JUMPER),
+					PERIPH_BIT_TO_STR(decoded.sys_status, PERIPH_SERVO)
+				);
+			}
+			else
+			{
+				printf("package dropped!\n");
 			}
 
-			char str_state[32];
-			system_state_to_str(str_state, decoded.sys_state);
-
-			char str_area[32];
-			system_area_to_str(str_area, decoded.sys_area);
-
-			char str_id[3];
-			memcpy(str_id, &received_id, 2);
-			str_id[2] = '\0';
-
-			printf("\x1b[48;2;89;123;202m / id: %s / ms: %lu \x1b[48;2;184;55;177m/ state: %s /\x1b[0m\x1b[48;2;89;123;202m area: %s / temp: %f / pressure: %f / altitude: %f / acc: (%.4f, %.4f, %.4f) / gps: (alt %.4f, lat %.4f, lon %.4f)\x1b[0m\n", str_id, time_ms, str_state, str_area, decoded.temp, decoded.pressure, decoded.altitude, decoded.acc_x, decoded.acc_y, decoded.acc_z, decoded.gps.altitude, decoded.gps.latitude, decoded.gps.longitude);
-			printf("periperals | radio: %s / sd: %s / barom: %s / acc: %s / gps: %s / jumper: %s / servo: %s\n\n",
-				PERIPH_BIT_TO_STR(decoded.sys_status, PERIPH_RADIO),
-				PERIPH_BIT_TO_STR(decoded.sys_status, PERIPH_SD),
-				PERIPH_BIT_TO_STR(decoded.sys_status, PERIPH_BAROM),
-				PERIPH_BIT_TO_STR(decoded.sys_status, PERIPH_ACC),
-				PERIPH_BIT_TO_STR(decoded.sys_status, PERIPH_GPS),
-				PERIPH_BIT_TO_STR(decoded.sys_status, PERIPH_JUMPER),
-				PERIPH_BIT_TO_STR(decoded.sys_status, PERIPH_SERVO)
-			);
+			curr_state = STATE_UKNOWN;
 		}
-		else
-		{
-			printf("package dropped!\n");
-		}
-		
 	}
 }
